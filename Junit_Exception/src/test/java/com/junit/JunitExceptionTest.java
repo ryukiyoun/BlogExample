b4160fa96ccf05a7ee4aa24751378fb53373063f
ryukiyoun <ryukiyoun@naver.com>
2021-12-28 00:15:23 +0900

    feat : Junit Exception 예제 등록
    - Junit5를 이용하여 Exception Throw Test 예제 테스트 예제 추가

--- Junit_Exception/src/test/java/com/junit/JunitExceptionTest.java
+++ Junit_Exception/src/test/java/com/junit/JunitExceptionTest.java
@@ -3,6 +3,7 @@ package com.junit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JunitExceptionTest {
@@ -10,18 +11,40 @@ public class JunitExceptionTest {
     @Test
     public void NullPointException(){
         Assertions.assertThrows(NullPointerException.class, () -> {
-            List<String> temp = null;
+            List<String> list = null;
 
-            temp.get(0);
+            list.get(0);
         });
     }
 
     @Test
     public void SupperClassRuntimeException(){
         Assertions.assertThrows(RuntimeException.class, () -> {
-            List<String> temp = null;
+            List<String> list = null;
 
-            temp.get(0);
+            list.get(0);
         });
+
+        Assertions.assertThrows(Exception.class, () -> {
+            List<String> list = null;
+
+            list.get(0);
+        });
+
+        Assertions.assertThrows(Throwable.class, () -> {
+            List<String> list = null;
+
+            list.get(0);
+        });
+    }
+
+    @Test
+    public void NonNullPointExceptionMessage(){
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            List<String> list = new ArrayList<>();
+            list.add("test1");
+
+            list.get(0);
+        }, "예외가 발생하지 않았습니다.");
     }
 }
