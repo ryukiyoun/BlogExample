8bed1f3027252e124b44baf71d33ec5938fcc9a8
ryukiyoun <ryukiyoun@naver.com>
2022-04-27 00:27:02 +0900

    feat : 섯다 게임 순수 Java개발
    - Combination algorithm 중 최종 Combination결과를 추가하는 Extract Method 진행

--- Practive_Stake/src/main/java/game/combination/CombinationCards.java
+++ Practive_Stake/src/main/java/game/combination/CombinationCards.java
@@ -9,35 +9,37 @@ import java.util.Set;
 
 public class CombinationCards {
     private final Set<List<Card>> combinationHand;
-    private List<Card> combinationSelect;
 
     public CombinationCards() {
-        this.combinationSelect = new ArrayList<>();
         this.combinationHand = new HashSet<>();
     }
 
     public Set<List<Card>> calcCombination(List<Card> cards, boolean[] isConfirm, int index, int count){
         combinationCards(cards, isConfirm, index, count);
-
         return combinationHand;
     }
 
     private void combinationCards(List<Card> hand, boolean[] checks, int index, int count) {
         if (count == 0) {
-            combinationSelect = new ArrayList<>();
-
-            for (int i = 0; i < hand.size(); i++) {
-                if (checks[i])
-                    combinationSelect.add(hand.get(i));
-            }
-
-            combinationHand.add(combinationSelect);
-        } else {
-            for (int i = index; i < hand.size(); i++) {
-                checks[i] = true;
-                combinationCards(hand, checks, i + 1, count - 1);
-                checks[i] = false;
-            }
+            combinationHand.add(getCombinationCardList(hand, checks));
+            return;
         }
+
+        for (int i = index; i < hand.size(); i++) {
+            checks[i] = true;
+            combinationCards(hand, checks, i + 1, count - 1);
+            checks[i] = false;
+        }
+    }
+
+    private List<Card> getCombinationCardList(List<Card> hand, boolean[] checks) {
+        List<Card> combinationSelect = new ArrayList<>();
+
+        for (int i = 0; i < hand.size(); i++) {
+            if (checks[i])
+                combinationSelect.add(hand.get(i));
+        }
+
+        return combinationSelect;
     }
 }
